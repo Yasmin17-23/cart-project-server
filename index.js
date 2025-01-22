e0b3155c8f2e03915c9b43d3539d67c4ae44247d
Yasmin17-23 <zyasmins1747@gmail.com>
2025-01-22 12:42:36 +0600

    created all file

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 //middleware
 const corsOptions = {
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174'
+        'http://localhost:5174',
+        'https://cart-project-77889.web.app',
       ],
      credentials: true,
      optionalSuccessStatus: 200,
@@ -35,6 +36,44 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+     const productsCollection = client.db("shopSlick").collection("products");
+     const cartsCollection = client.db("shopSlick").collection("carts");
+
+     //Get all products from db
+     app.get('/products', async(req, res) => {
+        const search = req.query.search || '';
+        
+        let query = {
+            title: { $regex: search, $options: 'i'},
+        }
+        const result = await productsCollection.find(query).toArray();
+        res.send(result);
+     })
+     
+     //carts collection
+     app.get('/carts', async (req, res) => {
+       const email = req.query.email;
+       const query = { email: email };
+       const result = await cartsCollection.find(query).toArray();
+       res.send(result);
+     })
+
+
+     app.post('/carts', async (req, res) => {
+       const cartItem = req.body;
+       const result = await cartsCollection.insertOne(cartItem);
+       res.send(result);
+     })
+    
+    app.delete('/cart/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId (id) };
+      const result = await cartsCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
